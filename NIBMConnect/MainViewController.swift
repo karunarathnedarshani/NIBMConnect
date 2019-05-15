50e961a3e10799ca5565dae27f10d3bb5d329726
Darshani Karunarathne <karunarathne.chamila@gmail.com>
2019-05-15 22:56:43 +0530

    Initial Commit Created Splash Screen

---
+++ NIBMConnect/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController.swift
+//  NIBMConnect
+//
+//  Created by Darshani Karunarathne on 5/15/19.
+//  Copyright © 2019 Darshani Karunarathne. All rights reserved.
+//
+
+import UIKit
+
+class UIViewController: UICollectionViewCell {
+    
+}
---
+++ NIBMConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBMConnect
+//
+//  Created by Darshani Karunarathne on 5/15/19.
+//  Copyright © 2019 Darshani Karunarathne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
